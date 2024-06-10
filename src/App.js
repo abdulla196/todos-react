dd3be4747f8f0712efe3650ebd757754e5b1e838
Abdullah Amr <32845266+abdulla196@users.noreply.github.com>
2024-06-10 16:53:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const[todos,setTodos]=useState([])
+  const inputRef = useRef();
+  const handlerInput = ()=>{
+    const text= inputRef.current.value;
+    const newItem = {completed:false , text};
+    setTodos([...todos,newItem]);
+    inputRef.current.value = ''
+  }
+  const handleItemTodo = (index)=> {
+    const newItems =[...todos];
+    newItems[index].completed = !newItems[index].completed;
+    setTodos(newItems);
+  }
+  const handleItemDelete = (index)=>{
+    const Items =[...todos];
+    Items.splice(index,1);
+    setTodos(Items)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1 className='title'>
+          Todo List 
+        </h1>
+        <div className='card'>
+          <input type='text' ref={inputRef}/>
+          <button  onClick={handlerInput}>Add Todos</button>
+          <ul>
+            {todos.map((item, index) => (
+              <li
+                key={index}
+                onClick={() => handleItemTodo(index)}
+                style={{ textDecoration: item.completed ? 'line-through' : 'none' }}
+              >
+                <p>
+                <span onClick={() => handleItemTodo(index)} style={{ cursor: 'pointer' }}>
+                  {item.text}
+                </span>
+                <span 
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleItemDelete(index);
+                  }} 
+                  style={{ marginLeft: '10px' }}
+                >
+                  ❌
+                </span>
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
